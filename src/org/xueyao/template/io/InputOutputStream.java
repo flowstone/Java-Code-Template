7a7a877a9e68d3ddd9f291b39cc3461a1f5b9f3d
flowstone <xueyao.me@gmail.com>
2017-08-15 10:58:51 +0800

    字节输出流缓冲区、字节输入流缓冲区

--- src/org/xueyao/template/io/InputOutputStream.java
+++ src/org/xueyao/template/io/InputOutputStream.java
@@ -1,5 +1,7 @@
 package org.xueyao.template.io;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -40,5 +42,39 @@ public class InputOutputStream {
         fos.close();
     }
     
+    /**
+     * 字节输入流缓冲区操作模板
+     * @param path 文件路径 
+     * @throws IOException
+     */
+    public static void bufferedInputStream(String path) throws IOException {
+        //创建输入流缓冲区对象
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(path));
+        //创建数组
+        byte[] b = new byte[1024];
+        //定义一个变量
+        int len = -1;
+        while ((len = bis.read(b)) != -1) {
+            System.out.println(new String(b, 0, len));
+        }
+        //释放资源
+        bis.close();
+    }
     
+    /**
+     * 字节输出流缓冲区操作
+     * @param path
+     * @throws IOException
+     */
+    public static void bufferedOutputStream(String path) throws IOException {
+        //创建输入流缓冲区对象 
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(path));
+        //获得当前系统的换行符
+        String lineSeparator = System.lineSeparator();
+        bos.write((lineSeparator+"测试代码").getBytes());
+        //将缓冲区中的数据刷新到输出流中
+        bos.flush();
+        //释放资源
+        bos.close();
+    }
 }
