9178791a1ec4303c94d7a6e7460173b0e18f3d6f
flowstone <xueyao.me@gmail.com>
2017-08-19 08:56:36 +0800

    事务-修复完成

--- src/org/xueyao/template/jdbc/transaction/AccountService02.java
+++ src/org/xueyao/template/jdbc/transaction/AccountService02.java
@@ -26,7 +26,7 @@ public class AccountService02 {
             //提交事务
             //con.commit();
             //使用dbutils管理事务
-            DbUtils.rollbackAndClose(con);
+            DbUtils.commitAndClose(con);
             System.out.println("转账成功");
             
         } catch (SQLException e) {
